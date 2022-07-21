71208c8a0e2221cc77eb31598c2a581f98b3fe7e
Arseniy <aben@yandex.ru>
2022-07-21 23:50:03 +0300

    Выполнена задание №64

--- Ex.64/Program.cs
+++ Ex.64/Program.cs
@@ -4,3 +4,17 @@
 M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 M = 4; N = 8. -> ""4, 6, 7, 8""
 */
+
+string recursion(int m, int n)
+{
+    if(m == n)
+        return n.ToString();
+    else
+        return m + ", " + recursion(m+1,n) ;
+}
+Console.WriteLine("введите число M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число N");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{recursion(m,n)} ");
